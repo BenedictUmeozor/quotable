3dc0c0a60f3701ce3f8ec751e8811f6ddc0eee59
Benedict <benedictumeozor@gmail.com>
2024-11-20 23:12:18 +0100

    backend complete

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,66 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import z from 'zod';
+import { quoteSchema } from './lib/zod';
+import axios, { type AxiosError, type AxiosResponse } from 'axios';
 
 dotenv.config();
+
+type Quote = z.infer<typeof quoteSchema>;
+const favorites: Quote[] = [];
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+app.get('/quote', async (req: Request, res: Response) => {
+  try {
+    const { data }: AxiosResponse<Quote> = await axios.get(
+      'https://dummyjson.com/quotes/random'
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    const errorData = (error as AxiosError).response?.data as {
+      error?: string;
+    };
+    res
+      .status(500)
+      .json({ error: errorData?.error || 'Internal Server Error' });
+  }
+});
+
+app.post('/quote', async (req: Request, res: Response) => {
+  try {
+    const quote = quoteSchema.parse(req.body);
+    const exists = favorites.find(f => f.id === quote.id);
+
+    if (exists) throw new Error('Quote already exists');
+
+    favorites.push(quote);
+    res.status(200).json({ message: 'Added to favorites' });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).json({
+        message: 'Validation error',
+        details: error.errors,
+      });
+    } else if (error instanceof Error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    } else {
+      res.status(500).json({
+        message: 'Internal Server Error',
+      });
+    }
+  }
+});
 
 const PORT = parseInt(process.env.PORT || '3000');
 
